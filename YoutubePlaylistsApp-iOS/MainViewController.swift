affdb34e448b899b767b5582ad936c9ffbc9e91b
Pavel Palancica <ppalancica@Pavels-MacBook-Pro.local>
2022-09-01 23:50:22 +0300

    Removed the SceneDelegate and Main storyboard related files

--- YoutubePlaylistsApp-iOS/MainViewController.swift
+++ YoutubePlaylistsApp-iOS/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  YoutubePlaylistsApp-iOS
 //
 //  Created by Pavel Palancica on 9/1/22.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Main"
+        view.backgroundColor = .red
     }
-
-
+    
 }
-
